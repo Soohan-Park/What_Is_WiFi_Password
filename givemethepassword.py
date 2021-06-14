d328ea00c01483aa55c212416d0b61f7ef523570
725psh <soohan.park@cj.net>
2021-06-14 11:18:21 +0900

    Fix Logic :: 패스워드를 못 찾았을 경우에 대한 예외처리

--- givemethepassword.py
+++ givemethepassword.py
@@ -2,7 +2,10 @@ import os
 
 
 def main():
-    os.system("clear")
+    # Shell별 상이한 명령어로 인해 clear 명령 수행 생략.
+    # os.system("clear")
+
+    print("=== GIVE ME THE PASSWORD ===")
 
     WiFiName = os.popen("netsh wlan show interfaces")
     while True:
@@ -10,16 +13,22 @@ def main():
         if "SSID" in target:
             target = target.split(':')[1].strip()
             break
-    
+
+    print("SSID :: {}".format(target))
+
+    finalKey = None
     data = os.popen("""netsh wlan show profile name="{}" key=clear""".format(target))
-    while True:
-        finalKey = data.readline()
-        if "키 콘텐츠" in finalKey:
+    for _ in range(100):
+        dataLine = data.readline()
+        if "키 콘텐츠" in dataLine:
             finalKey = finalKey.split(':')[1].strip()
             break
-    
-    print("지금 WiFi 비밀번호 : {}".format(finalKey))
-    os.system("Pause")
+
+    if finalKey is None:
+        print("현재 네트워크에 사용되고 있는 패스워드를 찾지 못하였습니다!")
+    else:
+        print("지금 WiFi 비밀번호 : {}".format(finalKey))
+        os.system("Pause")
 
 
 if __name__ == "__main__":
